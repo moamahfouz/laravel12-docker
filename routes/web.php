a974030f984189af5064b20598afacd38620fbb0
Mohamed Mahfouz <moa.mahfouz@gmail.com>
2026-05-08 20:51:34 +0000

    connect with node internally

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/node-data', function () {
+    try {
+        $response = Http::get('http://node:3000');
+        return response()->json($response->json());
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
+    }
 });
